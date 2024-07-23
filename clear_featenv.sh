ce5f31091021e83cc3143d1114870a54997c083a
Wei-Ming Tsai <wmtsai@jupyter.atmos.ucla.edu>
2024-07-23 11:49:47 -0700

    repo update

---
+++ clear_featenv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash/
+
+while getopts n: flag
+do
+    case "${flag}" in
+        n) name=${OPTARG}
+    esac
+done
+
+echo "Delete feature-environment dataset under: $name? (y/n)"
+read ans
+if [[ "$ans" -eq y ]]; then 
+  echo "delete files.."
+  rm -r ./dataset/$name
+fi
